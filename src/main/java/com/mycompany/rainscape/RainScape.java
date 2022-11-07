79af2e436192a67fb137b44da7de3858ae21254c
Zekiru <zekiel.villasurda@gmail.com>
2022-11-07 22:08:46 +0800

    Reworked and cleaned LoginGUI code
    Reworked code in LoginGUI into RainScape.java with more shorter but concise code

--- src/main/java/com/mycompany/rainscape/RainScape.java
+++ src/main/java/com/mycompany/rainscape/RainScape.java
@@ -5,6 +5,12 @@
 
 package com.mycompany.rainscape;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Ezekiel
@@ -19,6 +25,69 @@ public class RainScape {
         } catch (Exception e) {
             System.out.println("Project Not Launched.");
             System.out.println("Exception: " + e);
+        }
+    }
+    
+    public static void loginProcess(boolean login, String username, String password) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rainscape_db","root","");
+            String sql;
+            
+            if (login)
+                sql ="select * from rs_accounts where username=? and password=?";
+            else
+                sql ="select * from rs_accounts where username=?";
+            
+            PreparedStatement psmt = conn.prepareStatement(sql);
+            
+            if (login)
+                psmt.setString(2, password);
+            
+            psmt.setString(1, username);
+            
+            ResultSet rs = psmt.executeQuery();
+            
+            if (username.equals("") || password.equals(""))
+                JOptionPane.showMessageDialog(null,"Fill In The Empty Fields.");
+            else
+                if (rs.next()==true)
+                    if (login) {
+                        // Call next GUI here:
+                        
+                        JOptionPane.showMessageDialog(null,"Login Successful.");
+                    } else {
+                        JOptionPane.showMessageDialog(null,"Username Already Exists.");
+                    }
+                else
+                    if (login) {
+                        JOptionPane.showMessageDialog(null,"Login Failed.");
+                        // Call next GUI here
+                    } else {
+                        createAccount(username, password);
+                    }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,e);
+        }
+    }
+    
+    public static void createAccount(String username, String password) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rainscape_db","root","");
+            String sql = "insert into rs_accounts value ( ?, ? )";
+            
+            PreparedStatement psmt = conn.prepareStatement(sql);
+
+            psmt = conn.prepareStatement(sql);
+
+            psmt.setString(1, username);
+            psmt.setString(2, password);
+
+            psmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Account Created.");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
         }
     }
 }

a03c0f39a3632b0dddbeeb3ffc3be39503cacde8
Zekiru <zekiel.villasurda@gmail.com>
2022-11-07 09:33:30 +0800

    create project file

---
+++ src/main/java/com/mycompany/rainscape/RainScape.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.rainscape;
+
+/**
+ *
+ * @author Ezekiel
+ */
+public class RainScape {
+
+    public static void main(String[] args) {
+        System.out.println("Running Project...");
+        
+        try {
+            LoginGUI.main(args);
+            System.out.println("Project Launched.");
+        } catch (Exception e) {
+            System.out.println("Project Not Launched.");
+            System.out.println("Exception: " + e);
+        }
+    }
+}

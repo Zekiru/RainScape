f33073a5c24c143a900a47bc848b13377e5d11df
Zekiru <zekiel.villasurda@gmail.com>
2022-11-07 15:51:48 +0800

    update LoginGUI design and code

--- src/main/java/com/mycompany/rainscape/LoginGUI.java
+++ src/main/java/com/mycompany/rainscape/LoginGUI.java
@@ -34,10 +34,12 @@ public class LoginGUI extends javax.swing.JFrame {
 
         login_bg = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        username = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        password = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
+        username = new javax.swing.JTextField();
+        password = new javax.swing.JTextField();
+        jSeparator1 = new javax.swing.JSeparator();
+        jSeparator2 = new javax.swing.JSeparator();
         login = new javax.swing.JButton();
         signup = new javax.swing.JButton();
 
@@ -53,25 +55,44 @@ public class LoginGUI extends javax.swing.JFrame {
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("Username");
 
-        username.setBackground(new java.awt.Color(68, 68, 68));
-        username.setForeground(new java.awt.Color(255, 255, 255));
-        username.setName(""); // NOI18N
-
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("Password");
 
-        password.setBackground(new java.awt.Color(68, 68, 68));
-        password.setForeground(new java.awt.Color(255, 255, 255));
-
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel3.setText("Log In / Sign Up");
 
-        login.setBackground(new java.awt.Color(0, 153, 153));
+        username.setBackground(new java.awt.Color(51, 51, 51));
+        username.setForeground(new java.awt.Color(255, 255, 255));
+        username.setBorder(null);
+        username.setCaretColor(new java.awt.Color(255, 255, 255));
+        username.setOpaque(true);
+        username.setSelectionColor(new java.awt.Color(0, 153, 153));
+
+        password.setBackground(new java.awt.Color(51, 51, 51));
+        password.setForeground(new java.awt.Color(255, 255, 255));
+        password.setBorder(null);
+        password.setCaretColor(new java.awt.Color(255, 255, 255));
+        password.setOpaque(true);
+        password.setSelectionColor(new java.awt.Color(0, 153, 153));
+
+        jSeparator1.setDebugGraphicsOptions(javax.swing.DebugGraphics.NONE_OPTION);
+        jSeparator1.setEnabled(false);
+        jSeparator1.setRequestFocusEnabled(false);
+        jSeparator1.setVerifyInputWhenFocusTarget(false);
+
+        jSeparator2.setDebugGraphicsOptions(javax.swing.DebugGraphics.NONE_OPTION);
+        jSeparator2.setEnabled(false);
+        jSeparator2.setRequestFocusEnabled(false);
+        jSeparator2.setVerifyInputWhenFocusTarget(false);
+
+        login.setBackground(new java.awt.Color(0, 204, 204));
         login.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         login.setForeground(new java.awt.Color(255, 255, 255));
         login.setText("Log In");
+        login.setBorder(null);
+        login.setFocusable(false);
         login.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 loginActionPerformed(evt);
@@ -82,6 +103,8 @@ public class LoginGUI extends javax.swing.JFrame {
         signup.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         signup.setForeground(new java.awt.Color(0, 204, 204));
         signup.setText("Sign Up");
+        signup.setBorder(null);
+        signup.setFocusable(false);
         signup.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 signupActionPerformed(evt);
@@ -92,16 +115,19 @@ public class LoginGUI extends javax.swing.JFrame {
         login_bg.setLayout(login_bgLayout);
         login_bgLayout.setHorizontalGroup(
             login_bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, login_bgLayout.createSequentialGroup()
+            .addGroup(login_bgLayout.createSequentialGroup()
                 .addGap(70, 70, 70)
-                .addGroup(login_bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(login, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(signup, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(password)
-                    .addComponent(username))
+                .addGroup(login_bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(password, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(username, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(login_bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(login, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(signup, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jSeparator1)
+                        .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(70, 70, 70))
         );
         login_bgLayout.setVerticalGroup(
@@ -109,32 +135,40 @@ public class LoginGUI extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, login_bgLayout.createSequentialGroup()
                 .addGap(50, 50, 50)
                 .addComponent(jLabel3)
-                .addGap(18, 18, 18)
+                .addGap(29, 29, 29)
                 .addComponent(jLabel1)
-                .addGap(7, 7, 7)
-                .addComponent(username, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(13, 13, 13)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(username, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addComponent(jLabel2)
-                .addGap(10, 10, 10)
-                .addComponent(password, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(44, 44, 44)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(password, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(46, 46, 46)
                 .addComponent(login, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(signup, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(50, 50, 50))
+                .addGap(60, 60, 60))
         );
-
-        username.getAccessibleContext().setAccessibleName("");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(login_bg, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, 0)
+                .addComponent(login_bg, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(login_bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(0, 0, 0)
+                .addComponent(login_bg, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0))
         );
 
         pack();
@@ -142,67 +176,68 @@ public class LoginGUI extends javax.swing.JFrame {
 
     private void loginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginActionPerformed
         // TODO add your handling code here:
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rainscape_db","root","");
-            String sql ="select * from rs_accounts where username=? and password=?";
-
-            PreparedStatement psmt = conn.prepareStatement(sql);
-
-            psmt.setString(1, username.getText());
-            psmt.setString(2, password.getText());
-
-            ResultSet rs = psmt.executeQuery();
-
-            if (rs.next()==true)
-            JOptionPane.showMessageDialog(null,"Login Successful.");
-            else if (username.getText().equals("") || password.getText().equals(""))
-            JOptionPane.showMessageDialog(null,"Fill In The Empty Fields.");
-            else
-            JOptionPane.showMessageDialog(null,"Login Failed.");
-
-        } catch(Exception e) {
-            JOptionPane.showMessageDialog(null,e);
-        }
+        proccess(true);
     }//GEN-LAST:event_loginActionPerformed
 
     private void signupActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_signupActionPerformed
         // TODO add your handling code here:
+        proccess(false);
+    }//GEN-LAST:event_signupActionPerformed
+
+    public void proccess(boolean login) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rainscape_db","root","");
-            String sql ="select * from rs_accounts where username=?";
-
-            PreparedStatement psmt = conn.prepareStatement(sql);
-
-            psmt.setString(1, username.getText());
-
-            ResultSet rs = psmt.executeQuery();
-
-            if (rs.next()==true)
-            JOptionPane.showMessageDialog(null,"Username Already Exists.");
-            else if (username.getText().equals("") || password.getText().equals(""))
-            JOptionPane.showMessageDialog(null,"Fill In The Empty Fields.");
+            String sql;
+            
+            if (login)
+                sql ="select * from rs_accounts where username=? and password=?";
             else
-            try {
-                sql ="insert into rs_accounts value ( ?, ? )";
-
-                psmt = conn.prepareStatement(sql);
-
+                sql ="select * from rs_accounts where username=?";
+            
+            PreparedStatement psmt = conn.prepareStatement(sql);
+            
+            if (login) {
                 psmt.setString(1, username.getText());
                 psmt.setString(2, password.getText());
-
-                psmt.executeUpdate();
-                JOptionPane.showMessageDialog(null,"Account Created.");
-            } catch(Exception e) {
-                JOptionPane.showMessageDialog(null,e);
+            } else {
+                psmt.setString(1, username.getText());
             }
+            
+            ResultSet rs = psmt.executeQuery();
+            
+            if (login)
+                if (rs.next()==true)
+                    JOptionPane.showMessageDialog(null,"Login Successful.");
+                else if (username.getText().equals("") || password.getText().equals(""))
+                    JOptionPane.showMessageDialog(null,"Fill In The Empty Fields.");
+                else
+                    JOptionPane.showMessageDialog(null,"Login Failed.");
+            else
+                if (rs.next()==true)
+                    JOptionPane.showMessageDialog(null,"Username Already Exists.");
+                else if (username.getText().equals("") || password.getText().equals(""))
+                    JOptionPane.showMessageDialog(null,"Fill In The Empty Fields.");
+                else
+                    try {
+                        sql ="insert into rs_accounts value ( ?, ? )";
 
-        } catch(Exception e) {
+                        psmt = conn.prepareStatement(sql);
+
+                        psmt.setString(1, username.getText());
+                        psmt.setString(2, password.getText());
+
+                        psmt.executeUpdate();
+                        JOptionPane.showMessageDialog(null,"Account Created.");
+                    } catch(Exception e) {
+                        JOptionPane.showMessageDialog(null,e);
+                    }
+                    
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(null,e);
         }
-    }//GEN-LAST:event_signupActionPerformed
-
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -212,6 +247,8 @@ public class LoginGUI extends javax.swing.JFrame {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
+        
+        /*
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -228,6 +265,8 @@ public class LoginGUI extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(LoginGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        */
+        
         //</editor-fold>
 
         /* Create and display the form */
@@ -242,6 +281,8 @@ public class LoginGUI extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
     private javax.swing.JButton login;
     private javax.swing.JPanel login_bg;
     private javax.swing.JTextField password;

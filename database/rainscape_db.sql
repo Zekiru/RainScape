879104fbc015bf461fdd4bc44462a55c76a75f48
Zekiru <zekiel.villasurda@gmail.com>
2022-11-16 16:15:53 +0800

    Update RainScape Database
    Including Default Area Feature for accounts

--- database/rainscape_db.sql
+++ database/rainscape_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 07, 2022 at 03:31 AM
+-- Generation Time: Nov 16, 2022 at 09:13 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -37,7 +37,33 @@ CREATE TABLE `rs_accounts` (
 --
 
 INSERT INTO `rs_accounts` (`username`, `password`) VALUES
-('Zeke', '2004');
+('Aya', 'Pangan'),
+('Jay', 'Malinis'),
+('Justin', 'Lu'),
+('Marcus', 'Singson'),
+('Zeke', 'Villasurda');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rs_preferences`
+--
+
+CREATE TABLE `rs_preferences` (
+  `username` varchar(24) NOT NULL,
+  `area` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `rs_preferences`
+--
+
+INSERT INTO `rs_preferences` (`username`, `area`) VALUES
+('Aya', 'Manila'),
+('Jay', 'Makati City'),
+('Justin', 'Quezon City'),
+('Marcus', 'Manila'),
+('Zeke', 'Quezon City');
 
 --
 -- Indexes for dumped tables
@@ -48,6 +74,22 @@ INSERT INTO `rs_accounts` (`username`, `password`) VALUES
 --
 ALTER TABLE `rs_accounts`
   ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `rs_preferences`
+--
+ALTER TABLE `rs_preferences`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `rs_preferences`
+--
+ALTER TABLE `rs_preferences`
+  ADD CONSTRAINT `username` FOREIGN KEY (`username`) REFERENCES `rs_accounts` (`username`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

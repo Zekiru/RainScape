83faf95410869af84af0c0f8733dee6ad19fa915
Zekiru <zekiel.villasurda@gmail.com>
2022-11-25 07:06:18 +0800

    Update Database

--- database/rainscape_db.sql
+++ database/rainscape_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 24, 2022 at 12:29 AM
+-- Generation Time: Nov 25, 2022 at 12:05 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -44,7 +44,7 @@ INSERT INTO `rs_accounts` (`username`, `password`) VALUES
 ('Marcus', 'Singson'),
 ('Mozes', 'Garcia'),
 ('Raden', 'Alvarez'),
-('Wilson', 'Tiu'),
+('User', 'password'),
 ('Zeke', 'Villasurda');
 
 -- --------------------------------------------------------
@@ -72,7 +72,7 @@ INSERT INTO `rs_preferences` (`username`, `area`, `temp_scale`, `dark_mode`) VAL
 ('Marcus', 'Marikina', 'fahrenheit', 0),
 ('Mozes', 'Hongkong', 'fahrenheit', 1),
 ('Raden', 'Manila', 'fahrenheit', 0),
-('Wilson', 'Manila', 'celsius', 0),
+('User', 'Makati', 'celsius', 0),
 ('Zeke', 'Quezon City', 'celsius', 1);
 
 -- --------------------------------------------------------
@@ -91,41 +91,6 @@ CREATE TABLE `rs_weatherlogs` (
   `clouds` varchar(24) NOT NULL,
   `humidity` varchar(24) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `rs_weatherlogs`
---
-
-INSERT INTO `rs_weatherlogs` (`username`, `date_time`, `area`, `status`, `temp_scale`, `temp`, `clouds`, `humidity`) VALUES
-('Zeke', '2022-11-24 04:05:46', 'Tacloban City, Philippines', 'Light rain shower', 'celsius', '24°C', '54', '92'),
-('Zeke', '2022-11-24 04:07:27', 'Tacloban City, Philippines', 'Light rain shower', 'celsius', '24°C', '54', '92'),
-('Zeke', '2022-11-24 04:07:44', 'Tacloban City, Philippines', 'Light rain shower', 'celsius', '24°C', '54', '92'),
-('Zeke', '2022-11-24 04:09:48', 'Tacloban City, Philippines', 'Light rain shower', 'celsius', '24°C', '54', '92'),
-('Zeke', '2022-11-24 04:13:25', 'Tacloban City, Philippines', 'Light rain shower', 'celsius', '24°C', '54', '92'),
-('Zeke', '2022-11-24 04:14:37', 'Tacloban City, Philippines', 'Light rain shower', 'celsius', '24°C', '54', '92'),
-('Zeke', '2022-11-24 04:16:11', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '27°C', '25', '79'),
-('Zeke', '2022-11-24 04:18:45', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '27°C', '25', '79'),
-('Zeke', '2022-11-24 04:19:21', 'Cavite, Philippines', 'Partly cloudy', 'celsius', '27°C', '25', '79'),
-('Zeke', '2022-11-24 04:20:04', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '27°C', '25', '79'),
-('Zeke', '2022-11-24 04:21:18', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '27°C', '25', '79'),
-('Zeke', '2022-11-24 04:21:38', 'Quezon City, Philippines', 'Partly cloudy', 'fahrenheit', '80°F', '25', '79'),
-('Zeke', '2022-11-24 04:22:51', 'Quezon City, Philippines', 'Partly cloudy', 'fahrenheit', '80°F', '25', '79'),
-('Zeke', '2022-11-24 04:23:16', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '27°C', '25', '79'),
-('Zeke', '2022-11-24 04:25:50', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '27°C', '25', '79'),
-('Zeke', '2022-11-24 04:28:20', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '27°C', '25', '79'),
-('Zeke', '2022-11-24 04:29:02', 'Brasilia, Brazil', 'Partly cloudy', 'celsius', '23°C', '75', '73'),
-('Zeke', '2022-11-24 04:29:12', 'Colorado Springs, USA', 'Partly cloudy', 'celsius', '12°C', '25', '5'),
-('Zeke', '2022-11-24 04:29:18', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '27°C', '25', '79'),
-('Zeke', '2022-11-24 04:30:40', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '27°C', '25', '79'),
-('Zeke', '2022-11-24 04:57:10', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '27°C', '25', '79'),
-('Zeke', '2022-11-24 05:08:40', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '27°C', '25', '79'),
-('Zeke', '2022-11-24 05:15:04', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '27°C', '25', '79'),
-('Zeke', '2022-11-24 05:59:28', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '27°C', '25', '79'),
-('Zeke', '2022-11-24 06:57:26', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '27°C', '25', '79'),
-('Zeke', '2022-11-24 07:09:10', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '27°C', '25', '79'),
-('Zeke', '2022-11-24 07:10:11', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '27°C', '25', '79'),
-('Zeke', '2022-11-24 07:24:03', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '28°C', '25', '74'),
-('Zeke', '2022-11-24 07:28:06', 'Quezon City, Philippines', 'Partly cloudy', 'celsius', '28°C', '25', '74');
 
 --
 -- Indexes for dumped tables
